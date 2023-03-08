55cdcfa6e899c2084ddaf4246b06fc19ab2d9e82
saadashraf123 <saadjewani888@gmail.com>
2023-03-08 17:50:23 +0500

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav mx-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                        <a className="nav-link" href="#portfolio">Portfolio <span className="sr-only">(current)</span></a>
                     </li>
                     <span></span>
                     <li className="nav-item">
